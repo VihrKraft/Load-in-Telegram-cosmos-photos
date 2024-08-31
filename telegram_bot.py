73e6496179b6f3a55e7b292b24f4a21b0e024be2
Vihr_Kraft <sanefedov2009@gmail.com>
2024-08-31 20:10:07 +0700

    Сделал автоматическую публикацию фотографий в Телеграм-канал

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,13 +1,28 @@
 import os
 import telegram
 from dotenv import load_dotenv
+import random
+import time
+import argparse
 
 
 def main():
     load_dotenv()
     chat_id = '@cosmos_photos_2_0'
     bot = telegram.Bot(token=os.environ['BOT_TOKEN'])
-    bot.send_photo(chat_id=chat_id, photo=open('images/nasa_apod_1.jpg', 'rb'))
+    parser = argparse.ArgumentParser(
+        description='Программа для автоматической загрузки изображений в Telegram'
+    )
+    parser.add_argument('--delay', help='Укажите время задержки между отправкой фотографий (в секундах)', type=int, default=14400)
+    args = parser.parse_args()
+    while True:
+        images = os.listdir("images")
+        random.shuffle(images)
+        for image in images:
+            file_path = f'images/{image}'
+            with open(file_path, 'rb') as file:
+                bot.send_photo(chat_id=chat_id, photo=file)
+            time.sleep(args.delay)
 
 
 if __name__ == '__main__':
